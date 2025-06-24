c6618e71f32c5b5d6eb762f5c75feed129b3e5ce
yanniszang <xiaoyanzang24@hotmail.com>
2025-06-24 12:45:01 -0400

    pgvector

--- server/embeddings.js
+++ server/embeddings.js
@@ -2,6 +2,7 @@ import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { OllamaEmbeddings } from "@langchain/ollama";
 import { Document } from '@langchain/core/documents';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { PGVectorStore } from '@langchain/community/vectorstores/pgvector';
 
 // Option 2: Using Ollama Embeddings
 const embedding = new OllamaEmbeddings({
@@ -9,7 +10,22 @@ const embedding = new OllamaEmbeddings({
     baseUrl: "http://localhost:11434", // default Ollama URL
   });
 
-export const vectorStore = new MemoryVectorStore(embedding);
+// PGvector
+const vectorStore = new PGVectorStore.initialize(embedding, {
+    postgresConnectionOptions: {
+        connectionString: process.env.DB_URL,
+    },
+    tableName: 'vector_store',
+    columns: {
+        idColumnName: 'id',
+        vectorColumnName: 'vector',
+        contentColumnName: 'content',
+        metadataColumnName: 'metadata',
+    },
+    distanceStrategy: 'cosine',
+})
+
+// export const vectorStore = new MemoryVectorStore(embedding);
 
 export const addDocumentsToVectorStore = async (documents) => {
 
